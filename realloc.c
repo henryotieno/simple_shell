b796d7033d8fb5b154fe0a2d24312ce6a16877f5
henryotieno <henrynyobange@gmail.com>
2023-05-18 06:54:45 -0700

    simple_shell

---
+++ realloc.c
@@ -0,0 +1,61 @@
+#include "shell.h"
+
+/**
+ * _memset - memory filler
+ * @n: no. of bytes for filling
+ * @s: memory pointer
+ * @b: byte to be filled
+ * Return: (s)
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		s[a] = b;
+	`return (s);
+}
+
+/**
+ * ffree - releases strings
+ * @pp: string of strings
+ */
+void ffree(char **pp)
+{
+	char **a = pp;
+
+	if (!pp)
+		return;
+	while (*pp)
+		free(*pp++);
+	free(a);
+}
+
+/**
+ * _realloc - reassigns memory blocks
+ * @ptr: pointer
+ * @old_size: prev byte size
+ * @new_size: curr byte size
+ * Return: old pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(ptr), NULL);
+	if (new_size == old_size)
+		return (ptr);
+
+	p = malloc(new_size);
+	if (!p)
+		return (NULL);
+
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		p[old_size] = ((char *)ptr)[old_size];
+	free(ptr);
+	return (p);
+}
---
+++ vars.c
@@ -0,0 +1,149 @@
+#include "shell.h"
+
+/**
+ * is_chain - determine character
+ * @info: par structure
+ * @buf: buffer
+ * @p: positional address
+ * Return: 1 for deli, 0 if not
+ */
+int is_chain(info_t *info, char *buf, size_t *p)
+{
+	size_t k = *p;
+
+	if (buf[k] == '|' && buf[k + 1] == '|')
+	{
+		buf[k] = 0;
+		k++;
+		info->cmd_buf_type = CMD_OR;
+	}
+	else if (buf[k] == '&' && buf[k + 1] == '&')
+	{
+		buf[k] = 0;
+		k++;
+		info->cmd_buf_type = CMD_AND;
+	}
+	else if (buf[k] == ';')
+	{
+		buf[k] = 0;
+		info->cmd_buf_type = CMD_CHAIN;
+	}
+	else
+		return (0);
+	*p = k;
+	return (1);
+}
+
+/**
+ * check_chain - chaining checker
+ * @p: positional address
+ * @info: par structure
+ * @len: buf length
+ * @buf: buffer
+ * @i: kick off pos.
+ * Return: Void
+ */
+void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
+{
+	size_t k = *p;
+
+	if (info->cmd_buf_type == CMD_AND)
+	{
+		if (info->status)
+		{
+			buf[i] = 0;
+			k = len;
+		}
+	}
+	if (info->cmd_buf_type == CMD_OR)
+	{
+		if (!info->status)
+		{
+			buf[i] = 0;
+			k = len;
+		}
+	}
+
+	*p = k;
+}
+
+/**
+ * replace_alias - alias replacer
+ * @info: par structure
+ * Return: 1 when changed, 0 if not
+ */
+int replace_alias(info_t *info)
+{
+	int a;
+	list_t *node;
+	char *p;
+
+	for (a = 0; a < 10; a++)
+	{
+		node = node_starts_with(info->alias, info->argv[0], '=');
+		if (!node)
+			return (0);
+		free(info->argv[0]);
+		p = _strchr(node->str, '=');
+		if (!p)
+			return (0);
+		p = _strdup(p + 1);
+		if (!p)
+			return (0);
+		info->argv[0] = p;
+	}
+	return (1);
+}
+
+/**
+ * replace_vars - vars replacer
+ * @info: par structure
+ * Return: 1 when changed, 0 if not
+ */
+int replace_vars(info_t *info)
+{
+	int a = 0;
+	list_t *node;
+
+	for (a = 0; info->argv[a]; a++)
+	{
+		if (info->argv[a][0] != '$' || !info->argv[a][1])
+			continue;
+
+		if (!_strcmp(info->argv[a], "$?"))
+		{
+			replace_string(&(info->argv[a]),
+					_strdup(convert_number(info->status, 10, 0)));
+			continue;
+		}
+		if (!_strcmp(info->argv[a], "$$"))
+		{
+			replace_string(&(info->argv[a]),
+					_strdup(convert_number(getpid(), 10, 0)));
+			continue;
+		}
+		node = node_starts_with(info->env, &info->argv[a][1], '=');
+		if (node)
+		{
+			replace_string(&(info->argv[a]),
+					_strdup(_strchr(node->str, '=') + 1));
+			continue;
+		}
+		replace_string(&info->argv[a], _strdup(""));
+
+	}
+	return (0);
+}
+
+/**
+ * replace_string - string replacer
+ * @old: initial string add
+ * @new: current string
+ * Return: 1 when changed, 0 if not
+ */
+int replace_string(char **old, char *new)
+{
+	free(*old);
+	*old = new;
+	return (1);
+}
